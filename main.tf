a8e425c4e72b59d39e43b0ae253765ce8178b099
Chris Dunlap <chris@roadkill.org>
2019-10-10 10:25:48 -0700

    making it mine

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ data "vsphere_network" "network" {
 }
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = "terraform-roger"
+  name             = "terraform-cd"
   resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
 
